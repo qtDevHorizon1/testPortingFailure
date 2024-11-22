80c9d2ca4f46af992056d14a65675068016885d9
NoReply Amazon <no-reply@amazon.com>
2024-11-22 22:34:47 +0000

    [Commit] Iteration 1 #errors = 9 <= 10.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/azureappservice-authentication-middleware-surisaga-main/AzureAppServiceAuthenticationMiddleware/AzureAppServiceAuthenticationOptions.cs][CS0246][9,57] The type or namespace name 'AuthenticationOptions' could not be found (are you missing a using directive or an assembly reference?)``.

--- azureappservice-authentication-middleware-surisaga-main/AzureAppServiceAuthenticationMiddleware/AzureAppServiceAuthenticationOptions.cs
+++ azureappservice-authentication-middleware-surisaga-main/AzureAppServiceAuthenticationMiddleware/AzureAppServiceAuthenticationOptions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
