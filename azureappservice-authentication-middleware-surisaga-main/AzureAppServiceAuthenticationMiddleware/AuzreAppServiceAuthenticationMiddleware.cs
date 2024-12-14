e02391aa0361c04781617c5f3a947df3e9f31851
NoReply Amazon <no-reply@amazon.com>
2024-12-14 23:56:16 +0000

    [Commit] Iteration 2 #errors = 7 <= 9.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/azureappservice-authentication-middleware-surisaga-main/AzureAppServiceAuthenticationMiddleware/AuzreAppServiceAuthenticationMiddleware.cs][CS0308][14,60] The non-generic type 'AuthenticationMiddleware' cannot be used with type arguments``.

--- azureappservice-authentication-middleware-surisaga-main/AzureAppServiceAuthenticationMiddleware/AuzreAppServiceAuthenticationMiddleware.cs
+++ azureappservice-authentication-middleware-surisaga-main/AzureAppServiceAuthenticationMiddleware/AuzreAppServiceAuthenticationMiddleware.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,19 +11,14 @@ using Microsoft.AspNetCore.Authentication;
 
 namespace Middleware.Authentication.AppService
 {
-    public class AuzreAppServiceAuthenticationMiddleware : AuthenticationMiddleware<AzureAppServiceAuthenticationOptions>
+    public class AuzreAppServiceAuthenticationMiddleware : AuthenticationMiddleware
     {
-        public AuzreAppServiceAuthenticationMiddleware(RequestDelegate next, 
-            IOptions<AzureAppServiceAuthenticationOptions> options, 
-            ILoggerFactory loggerFactory, 
-            UrlEncoder encoder) : base(next, options, loggerFactory, encoder)
+        public AuzreAppServiceAuthenticationMiddleware(RequestDelegate next,
+            IAuthenticationSchemeProvider schemes,
+            IOptions<AuthenticationOptions> options,
+            ILoggerFactory loggerFactory)
+            : base(next, schemes, options, loggerFactory)
         {
-
-        }
-
-        protected override AuthenticationHandler<AzureAppServiceAuthenticationOptions> CreateHandler()
-        {
-            return new AzureAppServiceAuthenticationHandler();
         }
     }
 }
